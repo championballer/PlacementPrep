e086d2679c85ab2ffcba9cda305812de3a2f9869
Shiv Kumar <championballer08@gmail.com>
2019-06-13 23:47:20 +0530

    Added ibit files

--- ibit/Day38 Graphs1/Valid Path.cpp
+++ ibit/Day38 Graphs1/Valid Path.cpp
@@ -2,122 +2,75 @@
 
 using namespace std;
 
-void mark(int ** matrix, int A, int B,int x, int y, int R)
+/*
+41 67 5 0
+17 16 12 0 40
+52 61 61 25 31
+*/
+
+/*
+40 51 3 9
+27 1 16
+2 45 7
+*/
+void mark(bool ** matrix, int A, int B,int x, int y, int dis, int R, bool ** visited)
 {
+	if(x<0 || x>A || y<0 || y>B)return;
+	if(dis>R)return;
+	if(visited[x][y])return;
 
-	queue<pair<pair<int, int>,int> > pendingNodes;
-
-	pendingNodes.push(make_pair(make_pair(x,y),0));
-
-	while(!pendingNodes.empty())
-	{
-		auto top = pendingNodes.front();
-		pendingNodes.pop();
-		int i = top.first.first;
-		int j = top.first.second;
-		int dis = top.second;
-		cout<<i<<" "<<j<<" "<<dis<<endl;
-		matrix[i][j] = 1;
-		if(dis==R)continue;
-		else
-		{
-			if(i+1<A)pendingNodes.push(make_pair(make_pair(i+1,j),dis+1));
-			if(j+1<B)pendingNodes.push(make_pair(make_pair(i,j+1),dis+1));
-			if(i+1<A && j+1<B)pendingNodes.push(make_pair(make_pair(i+1,j+1),dis+1));
-			if(i-1>=0)pendingNodes.push(make_pair(make_pair(i-1,j),dis+1));
-			if(j-1>=0)pendingNodes.push(make_pair(make_pair(i,j-1),dis+1));
-			if(i-1>=0 && j-1>=0)pendingNodes.push(make_pair(make_pair(i-1,j-1),dis+1));
-			if(i-1>=0 && j<B)pendingNodes.push(make_pair(make_pair(i-1,j+1),dis+1));
-			if(i<A && j-1>=A)pendingNodes.push(make_pair(make_pair(i+1,j-1),dis+1));
-		}
-
-
-	}
+	matrix[x][y] = 1;
+	visited[x][y] = 1;
+	mark(matrix,A,B,x+1,y,dis+1,R,visited);
+	mark(matrix,A,B,x,y+1,dis+1,R,visited);
+	mark(matrix,A,B,x+1,y+1,dis+1,R,visited);
+	mark(matrix,A,B,x-1,y,dis+1,R,visited);
+	mark(matrix,A,B,x,y-1,dis+1,R,visited);
+	mark(matrix,A,B,x-1,y-1,dis+1,R,visited);
+	mark(matrix,A,B,x+1,y-1,dis+1,R,visited);
+	mark(matrix,A,B,x-1,y+1,dis+1,R,visited);
+	visited[x][y] = 0;
 }
 
-bool bfs(int ** matrix, int A, int B)
+bool travel(bool ** matrix, int A, int B,bool ** visited)
 {
-	int ** visited = new int*[A+1];
-	for(int i=0;i<=A;i++)
-	{
-		visited[i] = new int[B+1];
-		memset(visited[i],0,sizeof(int)*B+1);
-	}
-
 	queue<pair<int,int> > pendingNodes;
-	if(matrix[0][0]==1)return false;
 	pendingNodes.push(make_pair(0,0));
-	visited[0][0] = 1;
+
 	while(!pendingNodes.empty())
 	{
 		pair<int,int> top = pendingNodes.front();
 		pendingNodes.pop();
+
 		int x = top.first;
 		int y = top.second;
+
+		if(x<0 || x>A || y<0 || y>B)continue;
+		if(visited[x][y])continue;
+		if(matrix[x][y])continue;
 		if(x==A && y==B)return true;
-		else{
-
-			if(y+1<B && !matrix[x][y+1] && !visited[x][y+1])
-			{
-				visited[x][y+1] = 1;
-				pendingNodes.push(make_pair(x,y+1));
-			}
-
-			if(x+1<A && !matrix[x+1][y] && !visited[x+1][y])
-			{
-				visited[x+1][y] = 1;
-				pendingNodes.push(make_pair(x+1,y));	
-			}
-
-			if(x-1>0 && !matrix[x-1][y] && !visited[x-1][y])
-			{
-				visited[x-1][y] = 1;
-				pendingNodes.push(make_pair(x-1,y));	
-			}
-
-			if(y-1>0 && !matrix[x][y-1] && !visited[x][y-1])
-			{
-				visited[x][y-1] = 1;
-				pendingNodes.push(make_pair(x,y-1));	
-			}
-
-			if(x+1<A && y+1<B && !matrix[x+1][y+1] && !visited[x+1][y+1])
-			{
-				visited[x+1][y+1] = 1;
-				pendingNodes.push(make_pair(x+1,y+1));	
-			}
-
-			if(x+1<A && y-1>0 && !matrix[x+1][y-1] && !visited[x+1][y-1])
-			{
-				visited[x+1][y-1] = 1;
-				pendingNodes.push(make_pair(x+1,y-1));	
-			}
-
-			if(x-1>0 && y+1<B && !matrix[x-1][y+1] && !visited[x-1][y+1])
-			{
-				visited[x-1][y+1] = 1;
-				pendingNodes.push(make_pair(x-1,y+1));	
-			}
-
-			if(x-1>0 && y-1>0 && !matrix[x-1][y-1] && !visited[x-1][y-1])
-			{
-				visited[x-1][y-1] = 1;
-				pendingNodes.push(make_pair(x-1,y-1));	
-			}
-		}
+		visited[x][y] = true;
+		pendingNodes.push(make_pair(x+1,y));
+		pendingNodes.push(make_pair(x,y+1));
+		pendingNodes.push(make_pair(x+1,y+1));
+		pendingNodes.push(make_pair(x-1,y));
+		pendingNodes.push(make_pair(x,y-1));
+		pendingNodes.push(make_pair(x-1,y-1));
+		pendingNodes.push(make_pair(x+1,y-1));
+		pendingNodes.push(make_pair(x-1,y+1));
 	}
+
 	return false;
 
-	
 }
 
 string solve(int A, int B, int C, int D, vector<int> &E, vector<int> &F) {
 
-	int ** matrix = new int*[A+1];
+	bool ** matrix = new bool*[A+1];
 	for(int i=0;i<=A;i++)
 	{
-		matrix[i] = new int[B+1];
-		memset(matrix[i],0,sizeof(int)*B+1);
+		matrix[i] = new bool[B+1];
+		memset(matrix[i],0,sizeof(bool)*(B+1));
 	}
 
 	queue<pair<int,int> > pendingNodes;
@@ -126,31 +79,35 @@ string solve(int A, int B, int C, int D, vector<int> &E, vector<int> &F) {
 		pendingNodes.push(make_pair(E[i],F[i]));
 	}
 
+
+	bool ** visited = new bool*[A+1];
+	for(int i=0;i<=A;i++)visited[i] = new bool[B+1];
+
 	while(!pendingNodes.empty())
 	{
 		pair<int,int> top = pendingNodes.front();
 		int x = top.first;
 		int y = top.second;
 		pendingNodes.pop();
-		mark(matrix,A,B,x,y,D);
+		mark(matrix,A,B,x,y,0,D,visited);
 	}
 
-	// for(int i=0;i<A;i++)
-	// {
-	// 	for(int j=0;j<B;j++)
-	// 	{
-	// 		cout<<matrix[i][j]<<" ";
-	// 	}
+	for(int i=0;i<=A;i++)
+	{
+		for(int j=0;j<=B;j++)
+		{
+			//if(matrix[i][j])
+			cout<<matrix[i][j]<<" ";
+		}
 
-	// 	cout<<endl;
-	// }
-	// cout<<"End"<<endl;
-
-	bool ans = bfs(matrix,A,B);
+		cout<<endl;
+	}
+	cout<<"End"<<endl;
+	bool ans = travel(matrix,A,B,visited);
 	if(ans)return "YES";
 	else return "NO";
 
-	
+	return "YES";	
 }
 
 
